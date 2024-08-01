8657e7c49393301411d902d2fd8c9ce9a0dc8ff6
Dimitar <dimitar.ivanov@mentormate.com>
2024-08-01 23:23:43 +0300

    Initialize next.js project

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to My Minimal Next.js App with TypeScript</h1>
+            <p>This is the home page.</p>
+        </div>
+    );
+};
+
+export default Home;
