23e43df6524755b9096d29a49153a697cbacef32
Dimitar Ivanov <quadcorebg@gmail.com>
2024-08-04 18:31:19 +0300

    Modified the _app and _document components to correctly set basic meta and link tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,18 +15,6 @@ function App({ Component, pageProps }: AppProps) {
           content='width=device-width, initial-scale=1.0, shrink-to-fit=no'
         />
         <title>Chinese Remainder Theorem Calculator</title>
-        <link
-          rel='stylesheet'
-          href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
-        />
-        <link
-          rel='stylesheet'
-          href='https://fonts.googleapis.com/css?family=Catamaran:500,700,900&amp;display=swap'
-        />
-        <link
-          rel='stylesheet'
-          href='https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css'
-        />
       </Head>
       <Layout>
         <Component {...pageProps} />
