0151760f5ad12abd17ee79cc3ccb227297e1c7d1
Dimitar Ivanov <quadcorebg@gmail.com>
2024-08-04 19:18:20 +0300

    Added theme based styles to theme switch button; Added changing icons based on the current theme

--- components/SwitchThemeButton.tsx
+++ components/SwitchThemeButton.tsx
@@ -22,11 +22,20 @@ const SwitchThemeButton: NextPage = () => {
           fill='none'
           stroke-linecap='round'
           stroke-linejoin='round'
-          className='icon icon-tabler icon-tabler-sun'
+          className='icon icon-tabler'
         >
-          <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
-          <path d='M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0'></path>
-          <path d='M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7'></path>
+          {theme === 'dark' ? (
+            <>
+              <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+              <path d='M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0'></path>
+              <path d='M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7'></path>
+            </>
+          ) : (
+            <>
+              <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+              <path d='M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z'></path>
+            </>
+          )}
         </svg>
       </button>
     </div>
